2e672f40e196ef8431cc66fe6b99d0099662d0a2
MattWeiChen <mattchen1021@gmail.com>
2025-07-06 15:32:07 -0700

    add more files

---
+++ backend/src/schema/types/index.ts
@@ -0,0 +1,17 @@
+/**
+ * PURPOSE: Central export file for all GraphQL types
+ * - Exports all GraphQL types and resolvers from individual files
+ * - Makes it easy to import everything with a single import statement
+ * - Keeps the main schema file clean and organized
+ */
+
+// Export all user-related types and resolvers
+export * from "./User";
+
+// Export all student record types and resolvers
+export * from "./StudentRecord";
+
+// Export all region and group types and resolvers
+export * from "./Region";
+
+// Add more exports here as you create new type files
